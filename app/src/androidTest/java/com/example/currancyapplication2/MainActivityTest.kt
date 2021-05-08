1cb6118e74dd88f6fb014801ed7b025035895ee9
v.matyukhin <silf083@gmail.com>
2021-05-08 13:50:49 +0700

    Test CurrencyApplication on Espresso

--- app/src/androidTest/java/com/example/currancyapplication2/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/currancyapplication2/MainActivityTest.kt
@@ -1,8 +1,10 @@
 package com.example.currancyapplication2
 
 
-import android.content.ClipData.Item
-import androidx.core.content.res.TypedArrayUtils.getString
+import android.content.ContentValues.TAG
+import android.os.SystemClock
+import android.util.Log
+import android.widget.TextView
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onData
@@ -23,7 +25,6 @@ import org.junit.runner.RunWith
 class MainActivityTest : TestCase() {
 
 
-
     @Test
     fun isDisplayedList() {
         ActivityScenario.launch(MainActivity::class.java)
@@ -34,24 +35,37 @@ class MainActivityTest : TestCase() {
         //onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(11).
         //perform(click())
     }
+
     @Test
-    fun checkButtonUptClick(){
+    fun checkButtonUptClick() {
         ActivityScenario.launch(MainActivity::class.java)
         onView(withId(R.id.updateCurrancyBtn)).perform(click());
     }
+
     @Test
-    fun ElementVisible(){
+    fun ElementVisible() {
         ActivityScenario.launch(MainActivity::class.java)
         //5 element is Visible
         onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(5)
     }
 
     @Test//(flake)
-    fun findUsd(){
+    fun findUsd() {
+        val result = 13.4885
+
         ActivityScenario.launch(MainActivity::class.java)
+        SystemClock.sleep(1500);
+
         onData(anything()).inAdapterView(withId(R.id.listView)).atPosition(10).perform(scrollTo(), click())
         onView(withId(R.id.inputConvertibleValue)).perform(typeText(1000.toString()))
+        onView(withId(R.id.inputConvertibleValue)).perform(closeSoftKeyboard());
 
+        onView(withId(R.id.convertButton)).check(matches(isDisplayed()))
         onView(withId(R.id.convertButton)).perform(click())
+
+        //assert result
+        onView(withId(R.id.afterConversionValue)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.afterConversionValue)).check(matches(withText(result.toString())))
     }
 }
